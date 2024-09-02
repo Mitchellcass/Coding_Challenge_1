ab5311298939f554216dec9691304cbe6c1af184
MitchCas <155246406+Mitchellcass@users.noreply.github.com>
2024-09-01 22:38:28 -0400

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,11 @@
+// Task 1 Variables and Data Types
+
+let customerName = "Bob Marley";
+const customerID = 115; 
+var isPremiumMember = false;
+
+console.log(customerName, typeof(customerName));
+console.log(customerID, typeof(customerID));
+console.log(isPremiumMember, typeof(isPremiumMember));
+
+
