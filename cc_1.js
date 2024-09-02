5568864754c58eaffaf452c3c6611925794077d9
MitchCas <155246406+Mitchellcass@users.noreply.github.com>
2024-09-01 22:38:57 -0400

    Task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -9,3 +9,13 @@ console.log(customerID, typeof(customerID));
 console.log(isPremiumMember, typeof(isPremiumMember));
 
 
+ // Task 2: Compound Data Types
+
+ let orderItems = ["Macbook Pro","Keyboard","Mouse"];
+ const  orderDetails = {
+        orderID: 123,
+        items: orderItems,
+        totalAmount: 2459.00,
+ }
+
+console.log(orderItems, orderDetails);
